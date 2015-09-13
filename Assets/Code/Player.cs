ab7b740d29996b2d619f434e71e6c9b32aa1c5f8
fisser <daniel_pels@hotmail.com>
2015-09-14 01:43:34 +0200

    First commit

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    private float m_score = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //beweeg de speler naar rechts
+        this.transform.position += Vector3.right * Time.deltaTime;
+    }
+}
